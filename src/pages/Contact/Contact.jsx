68d1275ed47485763a512a00f461b1b4f862de05
lucaslomeu <lucaslomeugomes@gmail.com>
2021-12-05 16:11:09 -0300

    Fix onSubmit contact Page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -11,23 +11,15 @@ const Contact = () => {
   let topic = document.querySelector('input.input-topic');
   let textarea = document.querySelector('textarea.input-textarea');
 
-  const resetFields = (e) => {
-    e.preventDefault();
+  const resetFields = () => {
     name.value = '';
     email.value = '';
     topic.value = '';
     textarea.value = '';
   };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-
-    if (
-      name.value === '' ||
-      email.value === '' ||
-      topic.value === '' ||
-      textarea.value === ''
-    ) {
+  const handleClick = () => {
+    if (name === '' || email === '' || topic === '' || textarea === '') {
       alert('Favor completar todos os campos!');
     } else {
       alert('Email enviado com sucesso!');
@@ -40,7 +32,11 @@ const Contact = () => {
       <Header />
       <div className="container-form">
         <h1 className="title-contact">Entre em contato conosco</h1>
-        <form action="">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
           <input
             className="input-name"
             type="text"
