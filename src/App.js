dbbc949247eddb8026b561a6c5289e71627dd489
lucaslomeu <lucaslomeugomes@gmail.com>
2021-12-05 12:46:29 -0300

    Teste new color InputSearch 3.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/sobre" element={<About />} />
         <Route path="/contato" element={<Contact />} />
       </Routes>
     </BrowserRouter>
